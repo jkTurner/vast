ea467d2f55942fd2e1aef9f54a6b520f61a1ebcd
unknown <jkturner107@gmail.com>
2025-04-04 12:37:11 +0700

    improved navbar responsiveness for footer section

--- app/components/header/NavLink.tsx
+++ app/components/header/NavLink.tsx
@@ -5,14 +5,17 @@ import styles from "./header.module.css"
 interface NavLinkProps {
     item: PathType;
     isFirst?: boolean;
+    isLast?: boolean;
     color: 'primary' | 'accent' | 'textKill';
     showHome?: boolean;
 }
 
-const NavLink = ({item, isFirst = false, color = "primary", showHome = false }: NavLinkProps) => {
+const NavLink = ({item, isFirst = false, isLast = false, color = "primary", showHome = false }: NavLinkProps) => {
     return (
-        <li className={`${!isFirst ? `border-l border-${color}` : ''}
-            ${isFirst && showHome ? 'pl-0' : 'pl-sm'}
+        <li className={`${!showHome && !isFirst ? `border-l border-${color}` : ''}
+            ${isFirst && showHome ? 'pl-0 mr-sm' : 'pr-sm mr-sm'}
+            ${showHome ? `pl-0 border-l-0 border-r border-${color}` : ''}
+            ${showHome && isLast ? 'border-r-0 pl-0' : ''}
             ${styles.navList} `}>
             <Link href={item.path} className={`uppercase text-sm text-${color}`}>
                 {item.name}
--- app/components/header/Navbar.tsx
+++ app/components/header/Navbar.tsx
@@ -12,9 +12,11 @@ const Navbar: React.FC<NavbarProps> = ({ color = "primary", showHome = false }) 
 
     return (
         <nav className="flex items-center text-sm font-medium">
-            <ul className="flex items-center">
+            <ul className={`flex items-center
+                    ${showHome ? 'flex-wrap gap-y-sm' : ''}
+                `}>
                 {links.map((item, index) => (
-                    <NavLink key={item.name} item={item} isFirst={index === 0} color={color} showHome={showHome} />
+                    <NavLink key={item.name} item={item} isFirst={index === 0} isLast={index === Paths.length - 1} color={color} showHome={showHome} />
                 ))}
             </ul>
         </nav>
