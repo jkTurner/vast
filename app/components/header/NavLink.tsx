07c90d1d51123847f68ac79346b1aba99afd507c
unknown <jkturner107@gmail.com>
2025-04-04 12:58:03 +0700

    fixed !showHome right margin on navLink

--- app/components/header/NavLink.tsx
+++ app/components/header/NavLink.tsx
@@ -12,8 +12,10 @@ interface NavLinkProps {
 
 const NavLink = ({item, isFirst = false, isLast = false, color = "primary", showHome = false }: NavLinkProps) => {
     return (
-        <li className={`${!showHome && !isFirst ? `border-l border-${color}` : ''}
-            ${isFirst && showHome ? 'pl-0 mr-sm' : 'pr-sm mr-sm'}
+        <li className={`${!showHome && !isFirst ? `border-l border-${color} pl-sm` : ''}
+            ${isFirst && showHome ? 'pl-0 mr-sm' : ''}
+            ${showHome ? 'pr-sm mr-sm' : ''}
+            ${!isFirst && !showHome ? 'mr-0' : ''}
             ${showHome ? `pl-0 border-l-0 border-r border-${color}` : ''}
             ${showHome && isLast ? 'border-r-0 pl-0' : ''}
             ${styles.navList} `}>
