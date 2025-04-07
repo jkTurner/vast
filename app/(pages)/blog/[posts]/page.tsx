44ba7a1c1c13c06a6190712cab43f22585c7302d
unknown <jkturner107@gmail.com>
2025-04-07 22:19:10 +0700

    trying separated type declaration in dynamic page

--- app/(pages)/blog/[posts]/page.tsx
+++ app/(pages)/blog/[posts]/page.tsx
@@ -2,33 +2,38 @@ import { BlogPostData } from "@/data/mockup/MockupData";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
-// Mark the params prop as a promise:
-const PostPage = async ({ params }: { params: Promise<{ posts: string }> }) => {
-  const { posts: slug } = await params;
-
-  const post = BlogPostData.find(item => item.slug === slug);
-
-  if (!post) {
-    notFound();
-  }
-
-  return (
-    <div className="p-md">
-      <h1 className="text-2xl font-bold mb-sm">{post.title}</h1>
-
-      <div className="w-full h-[300px] relative mb-md">
-        <Image
-          src={post.image}
-          alt={post.title}
-          fill
-          priority
-          className="object-cover w-full h-full"
-        />
-      </div>
-
-      <div className="text-base text-primary">{post.content}</div>
-    </div>
-  );
+type Params = {
+  posts: string;
 };
 
-export default PostPage;
+const PostPage = async ({ params }: { params: Promise<Params> }) => {
+    const { posts: slug } = await params;
+  
+    const post = BlogPostData.find(item => item.slug === slug);
+  
+    if (!post) {
+      notFound();
+    }
+  
+    return (
+      <div className="p-md">
+        <h1 className="text-2xl font-bold mb-sm">{post.title}</h1>
+  
+        <div className="w-full h-[300px] relative mb-md">
+          <Image
+            src={post.image}
+            alt={post.title}
+            fill
+            priority
+            className="object-cover w-full h-full"
+          />
+        </div>
+  
+        <div className="text-base text-primary">{post.content}</div>
+      </div>
+    );
+  };
+  
+  export default PostPage;
+  
+  
